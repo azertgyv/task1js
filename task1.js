e6f23c913feb935d185e872db0b25bd41df4d04d
azertgyv <atagiyev077@gmail.com>
2023-05-06 20:54:14 +0400

    task1js

---
+++ task1.js
@@ -0,0 +1,20 @@
+//  Yas task1
+// let birthday = prompt('Enter yor birthday: ')
+//  const date = new Date();
+//  const currenDate = date.getFullYear()
+// let age = currenDate - Number(birthday)
+//  alert(`Your age: ${age}`)
+
+// gozellestirme task2
+// let beautifulName = prompt('Adinizi daxil edin: ')
+// alert(`~ ~ ${beautifulName} ~ ~`)
+
+// duzbucaqli task3
+//  let height = + prompt('Enter is height: ')
+//  let weight = + prompt('Enter is weight: ')
+//  alert(`Duzbucaqlinin Peremetri:${2*(height+weight)} Duzbucaqlinin sahesi: ${height*weight}`)
+
+// km to mil task4
+// let km = +prompt('Kilometri daxil edin: ')
+// let mil = parseFloat(0.6214)
+// alert(`${km}KM=${km * mil} MIL`)
